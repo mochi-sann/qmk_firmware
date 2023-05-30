bee6498e5f9a888cd697ec4a0ecf7406bbe19ca3
mochi33i <44772513+mochi-sann@users.noreply.github.com>
2023-05-30 13:40:35 +0900

    add macro

--- keyboards/lily58/keymaps/mochi33i/keymap.c
+++ keyboards/lily58/keymaps/mochi33i/keymap.c
@@ -8,7 +8,8 @@ enum layer_number {
 };
 
 enum custom_keycodes {
-    MOOCHI_TERM = SAFE_RANGE ,
+    MC_TERM = SAFE_RANGE ,
+    MC_MAX
 };
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
@@ -50,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
  */
 [_LOWER] = LAYOUT(
   _______, KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                      KC_F6,  KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,
-  _______, MOOCHI_TERM, _______, _______, _______, _______,               KC_VOLD, KC_VOLU, KC_MUTE ,KC_BRID,  KC_BRIU, KC_F12,
+  _______, MC_TERM, MC_MAX, _______, _______, _______,                   KC_VOLD, KC_VOLU, KC_MUTE ,KC_BRID,  KC_BRIU, KC_F12,
   KC_GRV, KC_EXLM, KC_AT,   KC_HASH, KC_DLR,  KC_PERC,                   KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, KC_TILD,
   _______, _______, _______, _______, _______, _______, KC_LBRC, KC_RBRC, XXXXXXX, KC_UNDS, KC_PLUS, KC_LCBR, KC_RCBR, KC_PIPE,
                              _______, _______, _______, _______, _______,  _______, _______, _______
@@ -143,9 +144,9 @@ bool oled_task_user(void) {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
     switch (keycode) {
-    case MOOCHI_TERM:
+    case MC_TERM:
         if (record->event.pressed) {
-            // キーコード QMKBEST が押された時
+            // ターミナルを表示するマクロ
             register_code(KC_LSFT);
             register_code(KC_LCTL);
             register_code(KC_LGUI);
@@ -159,6 +160,24 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             // キーコード QMKBEST が放された時
         }
         break;
+    case MC_MAX:
+        if (record->event.pressed) {
+            // ウィンドウを最大化するマクロ
+            register_code(KC_LSFT);
+            register_code(KC_LCTL);
+            register_code(KC_LGUI);
+            register_code(KC_LALT);
+            register_code(KC_ENT);
+        } else {
+            unregister_code(KC_ENT);
+            unregister_code(KC_LSFT);
+            unregister_code(KC_LCTL);
+            unregister_code(KC_LGUI);
+            unregister_code(KC_LALT);
+            // キーコード QMKBEST が放された時
+        }
+        break;
+
 
     }
 
